5ced0342408a896137ac5344ee7d802789ac4a69
sahan <sahan.m.rajapaksha@gmail.com>
2023-05-12 00:08:13 +0530

    @smr1897

--- snake.py
+++ snake.py
@@ -23,7 +23,7 @@ while True:
 
     if start_button.draw(screen):
         TILE_SIZE  = 40
-        RANGE = (TILE_SIZE//2,WINDOW-TILE_SIZE//3,TILE_SIZE)
+        RANGE = (TILE_SIZE//2,WINDOW-TILE_SIZE//2,TILE_SIZE)
         get_random_position = lambda: [randrange(*RANGE),randrange(*RANGE)]
         snake = pg.rect.Rect([0,0,TILE_SIZE-2,TILE_SIZE-2])
         snake.center = get_random_position()
